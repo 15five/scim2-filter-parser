7f472178eb93e43a7635f68f25aeeb1c1a08bf3f
Paul Logston <paul.logston@gmail.com>
2019-03-29 17:41:29 -0400

    Add lexer and unit tests

---
+++ src/scim2filter/lexer.py
@@ -0,0 +1,225 @@
+"""
+   The operators supported in the expression are listed in Table 3.
+
+   +----------+-------------+------------------------------------------+
+   | Operator | Description | Behavior                                 |
+   +----------+-------------+------------------------------------------+
+   | eq       | equal       | The attribute and operator values must   |
+   |          |             | be identical for a match.                |
+   |          |             |                                          |
+   | ne       | not equal   | The attribute and operator values are    |
+   |          |             | not identical.                           |
+   |          |             |                                          |
+   | co       | contains    | The entire operator value must be a      |
+   |          |             | substring of the attribute value for a   |
+   |          |             | match.                                   |
+   |          |             |                                          |
+   | sw       | starts with | The entire operator value must be a      |
+   |          |             | substring of the attribute value,        |
+   |          |             | starting at the beginning of the         |
+   |          |             | attribute value.  This criterion is      |
+   |          |             | satisfied if the two strings are         |
+   |          |             | identical.                               |
+   |          |             |                                          |
+   | ew       | ends with   | The entire operator value must be a      |
+   |          |             | substring of the attribute value,        |
+   |          |             | matching at the end of the attribute     |
+   |          |             | value.  This criterion is satisfied if   |
+   |          |             | the two strings are identical.           |
+   |          |             |                                          |
+   | pr       | present     | If the attribute has a non-empty or      |
+   |          | (has value) | non-null value, or if it contains a      |
+   |          |             | non-empty node for complex attributes,   |
+   |          |             | there is a match.                        |
+   |          |             |                                          |
+   | gt       | greater     | If the attribute value is greater than   |
+   |          | than        | the operator value, there is a match.    |
+   |          |             | The actual comparison is dependent on    |
+   |          |             | the attribute type.  For string          |
+   |          |             | attribute types, this is a               |
+   |          |             | lexicographical comparison, and for      |
+   |          |             | DateTime types, it is a chronological    |
+   |          |             | comparison.  For integer attributes, it  |
+   |          |             | is a comparison by numeric value.        |
+   |          |             | Boolean and Binary attributes SHALL      |
+   |          |             | cause a failed response (HTTP status     |
+   |          |             | code 400) with "scimType" of             |
+   |          |             | "invalidFilter".                         |
+   |          |             |                                          |
+   | ge       | greater     | If the attribute value is greater than   |
+   |          | than or     | or equal to the operator value, there is |
+   |          | equal to    | a match.  The actual comparison is       |
+   |          |             | dependent on the attribute type.  For    |
+   |          |             | string attribute types, this is a        |
+   |          |             | lexicographical comparison, and for      |
+   |          |             | DateTime types, it is a chronological    |
+   |          |             | comparison.  For integer attributes, it  |
+   |          |             | is a comparison by numeric value.        |
+   |          |             | Boolean and Binary attributes SHALL      |
+   |          |             | cause a failed response (HTTP status     |
+   |          |             | code 400) with "scimType" of             |
+   |          |             | "invalidFilter".                         |
+   |          |             |                                          |
+   | lt       | less than   | If the attribute value is less than the  |
+   |          |             | operator value, there is a match.  The   |
+   |          |             | actual comparison is dependent on the    |
+   |          |             | attribute type.  For string attribute    |
+   |          |             | types, this is a lexicographical         |
+   |          |             | comparison, and for DateTime types, it   |
+   |          |             | is a chronological comparison.  For      |
+   |          |             | integer attributes, it is a comparison   |
+   |          |             | by numeric value.  Boolean and Binary    |
+   |          |             | attributes SHALL cause a failed response |
+   |          |             | (HTTP status code 400) with "scimType"   |
+   |          |             | of "invalidFilter".                      |
+   |          |             |                                          |
+   | le       | less than   | If the attribute value is less than or   |
+   |          | or equal to | equal to the operator value, there is a  |
+   |          |             | match.  The actual comparison is         |
+   |          |             | dependent on the attribute type.  For    |
+   |          |             | string attribute types, this is a        |
+   |          |             | lexicographical comparison, and for      |
+   |          |             | DateTime types, it is a chronological    |
+   |          |             | comparison.  For integer attributes, it  |
+   |          |             | is a comparison by numeric value.        |
+   |          |             | Boolean and Binary attributes SHALL      |
+   |          |             | cause a failed response (HTTP status     |
+   |          |             | code 400) with "scimType" of             |
+   |          |             | "invalidFilter".                         |
+   +----------+-------------+------------------------------------------+
+
+                       Table 3: Attribute Operators
+
+   +----------+-------------+------------------------------------------+
+   | Operator | Description | Behavior                                 |
+   +----------+-------------+------------------------------------------+
+   | and      | Logical     | The filter is only a match if both       |
+   |          | "and"       | expressions evaluate to true.            |
+   |          |             |                                          |
+   | or       | Logical     | The filter is a match if either          |
+   |          | "or"        | expression evaluates to true.            |
+   |          |             |                                          |
+   | not      | "Not"       | The filter is a match if the expression  |
+   |          | function    | evaluates to false.                      |
+   +----------+-------------+------------------------------------------+
+
+                        Table 4: Logical Operators
+
+
+   +----------+-------------+------------------------------------------+
+   | Operator | Description | Behavior                                 |
+   +----------+-------------+------------------------------------------+
+   | ( )      | Precedence  | Boolean expressions MAY be grouped using |
+   |          | grouping    | parentheses to change the standard order |
+   |          |             | of operations, i.e., to evaluate logical |
+   |          |             | "or" operators before logical "and"      |
+   |          |             | operators.                               |
+   |          |             |                                          |
+   | [ ]      | Complex     | Service providers MAY support complex    |
+   |          | attribute   | filters where expressions MUST be        |
+   |          | filter      | applied to the same value of a parent    |
+   |          | grouping    | attribute specified immediately before   |
+   |          |             | the left square bracket ("[").  The      |
+   |          |             | expression within square brackets ("["   |
+   |          |             | and "]") MUST be a valid filter          |
+   |          |             | expression based upon sub-attributes of  |
+   |          |             | the parent attribute.  Nested            |
+   |          |             | expressions MAY be used.  See examples   |
+   |          |             | below.                                   |
+   +----------+-------------+------------------------------------------+
+
+                        Table 5: Grouping Operators
+
+"""
+from sly import Lexer
+
+
+class SCIMLexer(Lexer):
+    tokens = {
+        # Attribute Operators
+        EQ, NE,
+        GT, GE, LT, LE,
+        CO, SW, EW, PR,
+
+        # Logical Operators
+        AND, OR, NOT,
+        FALSE, TRUE,
+        NULL,
+
+        # Grouping Operators
+        LPAREN, RPAREN,
+        LBRACKET, RBRACKET,
+
+        # Other
+        NUMBER,
+        COMP_VALUE,
+        ATTRNAME,
+        DOT,
+        SP,
+        SCHEMA_URI,
+    }
+
+    # Filters MUST be evaluated using the following order of operations, in
+    # order of precedence:
+    # 1.  Grouping operators
+    # 2.  Logical operators - where "not" takes precedence over "and",
+    #     which takes precedence over "or"
+    # 3.  Attribute operators
+
+    ignore = ' \t'
+
+    # Attribute Operators
+    EQ = r'(E|e)(Q|q)'
+    NE = r'(N|n)(E|e)'
+    CO = r'(C|c)(O|o)'
+    SW = r'(S|s)(W|w)'
+    EW = r'(E|e)(W|w)'
+    PR = r'(P|p)(R|r)'
+    GT = r'(G|g)(T|t)'
+    GE = r'(G|g)(E|e)'
+    LT = r'(L|l)(T|t)'
+    LE = r'(L|l)(E|e)'
+
+    # Logical Operators
+    AND = r'(A|a)(N|n)(D|d)'
+    OR = r'(O|o)(R|r)'
+    NOT = r'(N|n)(O|o)(T|t)'
+
+    # Grouping Operators
+    LPAREN = r'\('
+    RPAREN = r'\)'
+    LBRACKET = r'\['
+    RBRACKET = r'\]'
+
+    # compValue literals
+    # false / null / true / number / string
+    # Rules from https://tools.ietf.org/html/rfc7159
+    FALSE = r'false'
+    TRUE = r'true'
+    NULL = r'null'
+    NUMBER = r'[0-9]' # only support integers at this time
+
+    # attrPath parts
+    SCHEMA_URI = r'[a-zA-Z]+:[a-zA-Z0-9:\._-]+'
+    ATTRNAME = r'[a-zA-Z][a-zA-Z0-9_-]*'
+
+    # Other characters
+    DOT = r'\.'
+    SP = r' '
+
+    # Ignored pattern
+    ignore_newline = r'\n+'
+
+    # Extra action for newlines
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')
+
+    def error(self, t):
+        print(f"Illegal character '{t.value[0]}'")
+        self.index += 1
+
+    @_(r'"([^"]*)"')
+    def COMP_VALUE(self, t):
+        t.value = t.value.strip('"')
+        return t
+
---
+++ tests/test_lexer.py
@@ -0,0 +1,248 @@
+from unittest import TestCase
+
+from scim2filter.lexer import SCIMLexer
+
+
+class RFCExamples(TestCase):
+    def setUp(self):
+        self.lexer = SCIMLexer()
+
+    def get_token_tuples(self, query):
+        return self.lexer.tokenize(query)
+
+    def assertTokens(self, query, expected):
+        token_tuples = [
+            (token.type, token.value) for token in self.get_token_tuples(query)
+        ]
+        self.assertEqual(expected, token_tuples)
+
+    def test_username_eq(self):
+        query = 'userName eq "bjensen"'
+        expected = [
+            ('ATTRNAME', 'userName'),
+            ('EQ', 'eq'),
+            ('COMP_VALUE', 'bjensen'),
+        ]
+        self.assertTokens(query, expected)
+
+    def test_family_name_contains(self):
+        query = '''name.familyName co "O'Malley"'''
+        expected = [
+            ('ATTRNAME', 'name'),
+            ('DOT', '.'),
+            ('ATTRNAME', 'familyName'),
+            ('CO', 'co'),
+            ('COMP_VALUE', "O'Malley")
+        ]
+        self.assertTokens(query, expected)
+
+    def test_username_startswith(self):
+        query = 'userName sw "J"'
+        expected = [
+            ('ATTRNAME', 'userName'),
+            ('SW', 'sw'),
+            ('COMP_VALUE', 'J'),
+        ]
+        self.assertTokens(query, expected)
+
+    def test_schema_username_startswith(self):
+        query = 'urn:ietf:params:scim:schemas:core:2.0:User:userName sw "J"'
+        expected = [
+            ('SCHEMA_URI', 'urn:ietf:params:scim:schemas:core:2.0:User:userName'),
+            ('SW', 'sw'),
+            ('COMP_VALUE', 'J'),
+        ]
+        self.assertTokens(query, expected)
+
+    def test_title_has_value(self):
+        query = 'title pr'
+        expected = [
+            ('ATTRNAME', 'title'),
+            ('PR', 'pr')
+        ]
+        self.assertTokens(query, expected)
+
+    def test_meta_last_modified_gt(self):
+        query = 'meta.lastModified gt "2011-05-13T04:42:34Z"'
+        expected = [
+            ('ATTRNAME', 'meta'),
+            ('DOT', '.'),
+            ('ATTRNAME', 'lastModified'),
+            ('GT', 'gt'),
+            ('COMP_VALUE', '2011-05-13T04:42:34Z'),
+        ]
+        self.assertTokens(query, expected)
+
+    def test_meta_last_modified_ge(self):
+        query = 'meta.lastModified ge "2011-05-13T04:42:34Z"'
+        expected = [
+            ('ATTRNAME', 'meta'),
+            ('DOT', '.'),
+            ('ATTRNAME', 'lastModified'),
+            ('GE', 'ge'),
+            ('COMP_VALUE', '2011-05-13T04:42:34Z'),
+        ]
+        self.assertTokens(query, expected)
+
+    def test_meta_last_modified_lt(self):
+        query = 'meta.lastModified lt "2011-05-13T04:42:34Z"'
+        expected = [
+            ('ATTRNAME', 'meta'),
+            ('DOT', '.'),
+            ('ATTRNAME', 'lastModified'),
+            ('LT', 'lt'),
+            ('COMP_VALUE', '2011-05-13T04:42:34Z'),
+        ]
+        self.assertTokens(query, expected)
+
+    def test_meta_last_modified_le(self):
+        query = 'meta.lastModified le "2011-05-13T04:42:34Z"'
+        expected = [
+            ('ATTRNAME', 'meta'),
+            ('DOT', '.'),
+            ('ATTRNAME', 'lastModified'),
+            ('LE', 'le'),
+            ('COMP_VALUE', '2011-05-13T04:42:34Z'),
+        ]
+        self.assertTokens(query, expected)
+
+    def test_title_has_value_and_user_type_eq(self):
+        query = 'title pr and userType eq "Employee"'
+        expected = [
+            ('ATTRNAME', 'title'),
+            ('PR', 'pr'),
+            ('AND', 'and'),
+            ('ATTRNAME', 'userType'),
+            ('EQ', 'eq'),
+            ('COMP_VALUE', 'Employee')
+        ]
+        self.assertTokens(query, expected)
+
+    def test_title_has_value_or_user_type_eq(self):
+        query = 'title pr or userType eq "Intern"'
+        expected = [
+            ('ATTRNAME', 'title'),
+            ('PR', 'pr'),
+            ('OR', 'or'),
+            ('ATTRNAME', 'userType'),
+            ('EQ', 'eq'),
+            ('COMP_VALUE', 'Intern')
+        ]
+        self.assertTokens(query, expected)
+
+    def test_schemas_eq(self):
+        query = 'schemas eq "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"'
+        expected = [
+            ('ATTRNAME', 'schemas'),
+            ('EQ', 'eq'),
+            ('COMP_VALUE', 'urn:ietf:params:scim:schemas:extension:enterprise:2.0:User')
+        ]
+        self.assertTokens(query, expected)
+
+    def test_user_type_eq_and_email_contains_or_email_contains(self):
+        query = 'userType eq "Employee" and (emails co "example.com" or emails.value co "example.org")'
+        expected = [
+            ('ATTRNAME', 'userType'),
+            ('EQ', 'eq'),
+            ('COMP_VALUE', 'Employee'),
+            ('AND', 'and'),
+            ('LPAREN', '('),
+            ('ATTRNAME', 'emails'),
+            ('CO', 'co'),
+            ('COMP_VALUE', 'example.com'),
+            ('OR', 'or'),
+            ('ATTRNAME', 'emails'),
+            ('DOT', '.'),
+            ('ATTRNAME', 'value'),
+            ('CO', 'co'),
+            ('COMP_VALUE', 'example.org'),
+            ('RPAREN', ')')
+        ]
+        self.assertTokens(query, expected)
+
+    def test_user_type_ne_and_not_email_contains_or_email_contains(self):
+        query = 'userType ne "Employee" and not (emails co "example.com" or emails.value co "example.org")'
+        expected = [
+            ('ATTRNAME', 'userType'),
+            ('NE', 'ne'),
+            ('COMP_VALUE', 'Employee'),
+            ('AND', 'and'),
+            ('NOT', 'not'),
+            ('LPAREN', '('),
+            ('ATTRNAME', 'emails'),
+            ('CO', 'co'),
+            ('COMP_VALUE', 'example.com'),
+            ('OR', 'or'),
+            ('ATTRNAME', 'emails'),
+            ('DOT', '.'),
+            ('ATTRNAME', 'value'),
+            ('CO', 'co'),
+            ('COMP_VALUE', 'example.org'),
+            ('RPAREN', ')')
+        ]
+        self.assertTokens(query, expected)
+
+    def test_user_type_eq_and_not_email_type_eq(self):
+        query = 'userType eq "Employee" and (emails.type eq "work")'
+        expected = [
+            ('ATTRNAME', 'userType'),
+            ('EQ', 'eq'),
+            ('COMP_VALUE', 'Employee'),
+            ('AND', 'and'),
+            ('LPAREN', '('),
+            ('ATTRNAME', 'emails'),
+            ('DOT', '.'),
+            ('ATTRNAME', 'type'),
+            ('EQ', 'eq'),
+            ('COMP_VALUE', 'work'),
+            ('RPAREN', ')')
+        ]
+        self.assertTokens(query, expected)
+
+    def test_user_type_eq_and_not_email_type_eq_work_and_value_contains(self):
+        query = 'userType eq "Employee" and emails[type eq "work" and value co "@example.com"]'
+        expected = [
+            ('ATTRNAME', 'userType'),
+            ('EQ', 'eq'),
+            ('COMP_VALUE', 'Employee'),
+            ('AND', 'and'),
+            ('ATTRNAME', 'emails'),
+            ('LBRACKET', '['),
+            ('ATTRNAME', 'type'),
+            ('EQ', 'eq'),
+            ('COMP_VALUE', 'work'),
+            ('AND', 'and'),
+            ('ATTRNAME', 'value'),
+            ('CO', 'co'),
+            ('COMP_VALUE', '@example.com'),
+            ('RBRACKET', ']')
+        ]
+
+    def test_emails_type_eq_work_value_contians_or_ims_type_eq_and_value_contians(self):
+        query = ('emails[type eq "work" and value co "@example.com"] or '
+                 'ims[type eq "xmpp" and value co "@foo.com"]')
+        expected = [
+            ('ATTRNAME', 'emails'),
+            ('LBRACKET', '['),
+            ('ATTRNAME', 'type'),
+            ('EQ', 'eq'),
+            ('COMP_VALUE', 'work'),
+            ('AND', 'and'),
+            ('ATTRNAME', 'value'),
+            ('CO', 'co'),
+            ('COMP_VALUE', '@example.com'),
+            ('RBRACKET', ']'),
+            ('OR', 'or'),
+            ('ATTRNAME', 'ims'),
+            ('LBRACKET', '['),
+            ('ATTRNAME', 'type'),
+            ('EQ', 'eq'),
+            ('COMP_VALUE', 'xmpp'),
+            ('AND', 'and'),
+            ('ATTRNAME', 'value'),
+            ('CO', 'co'),
+            ('COMP_VALUE', '@foo.com'),
+            ('RBRACKET', ']')
+        ]
+        self.assertTokens(query, expected)
+
