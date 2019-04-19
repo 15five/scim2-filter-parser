723f4129632e37d3a0d1a6d88d9d7d5c13da6f4e
Paul Logston <paul.logston@gmail.com>
2019-04-18 23:49:03 -0400

    Start work on Query object

---
+++ TODO.rst
@@ -0,0 +1,9 @@
+- Add tests to bring up coverage (3hr)
+- Add tests for major IdP style filters (3hr)
+- Add docs in read me for how to install and use (3hr)
+- Add tests for passing `attr_map` to SQLTranspiler (2hr)
+- Add proper docs folder with sphinx (1hr)
+- Add project to readthedocs (1hr)
+- If attr_map value contains a "." and doesn't start with the name of the main table in a Query object, add joins.
+
+
---
+++ src/scim2_filter_parser/query.py
@@ -0,0 +1,78 @@
+"""
+The logic in this module builds a full SQL query based on a SCIM filter.
+"""
+from .lexer import SCIMLexer
+from .parser import SCIMParser
+from .transpiler import SCIMToSQLTranspiler
+
+
+class Query:
+    placeholder = '%s'
+
+    def __init__(self, filter_, table_name, attr_map):
+        self.filter: str = filter_
+        self.table_name: str = table_name
+        self.attr_map: dict = attr_map
+        self.where_sql: str = None
+        self.where_params: dict = None
+        self.params: list = None
+
+        self.build_where_sql()
+
+    def build_where_sql(self):
+        token_stream = SCIMLexer().tokenize(self.filter)
+        ast = SCIMParser().parse(token_stream)
+        sql, params = SCIMToSQLTranspiler(self.attr_map).transpile(ast)
+
+        self.where_sql = sql
+        self.where_params = params
+
+        self.params = [params.get(i) for i in range(len(params))]
+
+    @property
+    def sql(self) -> str:
+        # Replace {#} with placeholder string. Different database
+        # connectors can override this with their own placeholder character.
+        placeholders = [self.placeholder for i in range(len(self.params))]
+        where_sql = self.where_sql.format(*placeholders)
+        return f'SELECT * FROM {self.table_name} WHERE {where_sql};'
+
+    def __str__(self) -> str:
+        orig_placeholder = self.placeholder
+        self.placeholder = '{}'
+        sql = self.sql.format(*self.params)
+        self.placeholder = orig_placeholder
+
+        # Wrap the SQL in invalid characters so users don't accidentally
+        # walk into a SQL Injection vulnerability.
+        return '>>>' + sql + '<<<'
+
+
+class SQLiteQuery(Query):
+    placeholder = '?'
+
+
+def main():
+    '''
+    Main program. Used for testing.
+    '''
+    import sys
+
+    if len(sys.argv) != 2:
+        sys.stderr.write('Usage: python -m scim2_filter_parser.query <filter>\n')
+        raise SystemExit(1)
+
+    attr_map = {
+        ('username', None, None): 'users.username',
+        ('name', 'familyname', None): 'users.family_name',
+        ('meta', 'lastmodified', None): 'update_ts',
+    }
+
+    q = SQLiteQuery(sys.argv[1], 'users', attr_map)
+
+    print(q)
+
+
+if __name__ == '__main__':
+    main()
+
