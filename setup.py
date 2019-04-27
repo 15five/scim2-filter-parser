e217f2d48e2c29a50678219d3d75d76434a5376d
Paul Logston <paul.logston@gmail.com>
2019-04-26 23:44:39 -0400

    Version bump => 0.1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def long_description():
 
 setup(
     name='scim2-filter-parser',
-    version='0.0.2',
+    version='0.1.0',
     description='A customizable parser/transpiler for SCIM2.0 filters',
     url='https://github.com/15five/scim2-filter-parser',
     maintainer='Paul Logston',
